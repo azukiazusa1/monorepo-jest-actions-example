7f25f4c5cce7a8bbcd0cf59933e108dec224edd8
azukiazusa1 <59350345+azukiazusa1@users.noreply.github.com>
2022-11-29 10:39:37 +0900

    t

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,6 +2,12 @@ const add = (a, b) => {
   return a + b;
 };
 
+const divide = (a, b) => {
+  if (b === 0) throw new Error('division by 0')
+  return a / b
+}
+
 module.exports = {
   add,
+  divide
 };
